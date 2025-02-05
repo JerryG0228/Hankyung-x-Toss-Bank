abadec317c20c44f93b5132101979569b8707c5b
zoo <anfdnfl@khu.ac.kr>
2025-02-05 11:32:48 +0900

    리액트 연습문제 7

--- React/Intermediate/src/5-lifecycle,useEffect/Ex.jsx
+++ React/Intermediate/src/5-lifecycle,useEffect/Ex.jsx
@@ -65,8 +65,81 @@ export function Ex3() {
   return (
     <div>
       <div style={{ color: "tomato" }}>{timer}초</div>
-
-      
     </div>
+  );
+}
+
+// 4. 윈도우 크기 변경 감지
+// 목표: 윈도우의 크기가 변경될 때마다 현재 창의 너비를 화면에 표시하세요.
+// 요구사항:
+// useEffect로 이벤트 리스너를 등록하고 창 크기가 변경될 때마다 상태를 업데이트하세요.
+// 언마운트 시 이벤트 리스너를 해제하세요.
+// 힌트: window.addEventListener와 window.removeEventListener
+
+export function Ex4() {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleSizing = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleSizing);
+    return () => {
+      window.removeEventListener("resize", handleSizing);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>
+        width:{size.width}, height:{size.height}
+      </div>
+    </>
+  );
+}
+
+//연습문제5: API 호출 및 데이터 로드
+// 목표: 컴포넌트가 마운트될 때 API 호출을 통해 데이터를 가져와 화면에 표시하세요.
+// 요구사항:
+// 1. URL: https://jsonplaceholder.typicode.com/posts
+// 2. 상위 10개의 포스트 테이터만 출력하세요.
+// 3. useEffect를 사용하여 컴포넌트가 마운트될 때 데이터를 로드하세요.
+// 4. 데이터를 로드한 후 상태에 저장하고 화면에 출력하세요.
+// 힌트: fetch 또는 axios 모듈 사용 가능합니다.
+
+export function Ex5() {
+  const [dt, setDt] = useState([]);
+
+  useEffect(() => {
+    const fectchData = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((data) => {
+          const newData = data.filter((v, idx) => idx < 10);
+          setDt(newData);
+        });
+    };
+    fectchData();
+  }, []);
+
+  console.log(dt);
+  return (
+    <>
+      {dt.map((v) => {
+        return (
+          <div key={v.id}>
+            <h1>{v.title}</h1>
+            <div>{v.body}</div>
+          </div>
+        );
+      })}
+    </>
   );
 }
--- React/Intermediate/src/App.jsx
+++ React/Intermediate/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Ex1, Ex2, Ex3 } from "./5-lifecycle,useEffect/Ex";
+import { Ex1, Ex2, Ex3, Ex4, Ex5 } from "./5-lifecycle,useEffect/Ex";
 
 function App() {
   const [mount, setMount] = useState(true);
@@ -9,6 +9,8 @@ function App() {
       <Ex1 />
       <Ex2 />
       {mount && <Ex3 />}
+      {mount && <Ex4 />}
+      <Ex5 />
       <button onClick={() => setMount(!mount)}>마운트버튼</button>
     </div>
   );
