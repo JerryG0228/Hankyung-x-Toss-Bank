943f53388c3fcdf938f6423b487cebf0ac2bf7eb
zoo <anfdnfl@khu.ac.kr>
2025-02-25 09:07:38 +0900

    typescript 1

---
+++ Typescript/src/ex01.ts
@@ -0,0 +1,21 @@
+// 기본 타입(Primitive type) : 하나의 값만 가지는 타입
+
+// 명시적 타입선언 vs 암묵적 타입선언
+// let myNum1: number = 123
+// let myNum2 = 123
+
+// number
+let num1: number = 123;
+// string
+let str1: string = "hello";
+// boolean
+let bool1: boolean = true;
+// null
+let null1: null = null;
+// undefined
+let unde1: undefined = undefined;
+// 리터럴 타입
+let lit1: 10 = 10;
+
+//"strict": true 옵션 때문에 null 값 할당 안됨
+//let numA: number = null;
